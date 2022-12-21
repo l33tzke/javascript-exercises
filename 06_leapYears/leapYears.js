b6a1f0de7b597bbe3253a8ebc70bd9bfa845dcdc
Andre Leitzke <355407+l33tzke@users.noreply.github.com>
2022-12-21 23:48:23 +0100

    Solve exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
 const leapYears = function() {
+    let year = arguments[0];
+    if (year % 4 == 0){
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    } 
+    return false;
 
 };
 
