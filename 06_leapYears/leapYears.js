483e895af076bc3960ecf921d2aaf3d93d7075c2
Andre Leitzke <355407+l33tzke@users.noreply.github.com>
2022-12-21 23:52:08 +0100

    Improve solution for exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,6 @@
 const leapYears = function() {
     let year = arguments[0];
-    if (year % 4 == 0){
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true;
-            }
-            return false;
-        }
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
         return true;
     } 
     return false;
