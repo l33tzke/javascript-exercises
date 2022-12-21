b38f9df4191a787f3d06fe3cad870cc78c3283c0
Andre Leitzke <355407+l33tzke@users.noreply.github.com>
2022-12-21 23:33:19 +0100

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
+function areArgumentsValid(args){
+    if (args[0] < 0 || args[1] < 0) {return false;}
+    if (typeof(args[0]) != "number" || typeof(args[1]) != "number") {return false;}
+    return true;
+}
+
 const sumAll = function() {
-    let begin = arguments[0];
-    let end = arguments[1];
+    let begin, end;
+    if (!areArgumentsValid(arguments)) return "ERROR"; 
+    if (arguments[1] > arguments [0]){
+        begin = arguments[0];
+        end = arguments[1];
+    } else {
+        begin = arguments[1];
+        end = arguments[0];
+    }
     let sum = 0;
 
     for (let i = begin; i <= end; i++){
