279b146bd4d4da03aa350e6a67a818c91cf2c9a5
Andre Leitzke <355407+l33tzke@users.noreply.github.com>
2023-01-16 19:18:53 +0100

    Add solution for exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
 const fibonacci = function() {
+    let order = Number(arguments[0])
+    if (order < 0) return "OOPS";
+    
+    return fib(order);
 
+    function fib(order) {
+        if (order > 1) {
+            return fib(order-1) + fib(order-2);
+        } else if(order == 1) {
+            return 1
+        } 
+        else {
+            return 0;
+        }
+        
+    }
+    
 };
 
 // Do not edit below this line
