43779e23dd7bd8e72383d6e8951c9a31cd0dda69
Andre Leitzke <355407+l33tzke@users.noreply.github.com>
2023-01-16 18:58:01 +0100

    Add response to exercises 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
 const add = function() {
-	
+	return arguments[0]+arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0]-arguments[1];
 };
 
 const sum = function() {
-	
+	let numbers = arguments[0];
+  let sum = 0;
+  numbers.forEach(function(number) {
+    sum += number;
+  });
+  return sum;
 };
 
 const multiply = function() {
-
+  let numbers = arguments[0];
+  let total = 1;
+  numbers.forEach(function(number) {
+    total *= number;
+  });
+  return total;
 };
 
 const power = function() {
-	
+	let base = arguments[0];
+  let power = arguments[1];
+  let result = 1;
+  for (let i = 0; i < power; i++) {
+    result = result * base;
+  }
+  return result;
 };
 
 const factorial = function() {
-	
+	let number = arguments[0];
+  let result = 1;
+  for (let i = 0; i < number; i++) {
+    result = result * (i+1);
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
 const palindromes = function () {
 
+    let sanitizedInput = removeSpaces(removePunctuation(arguments[0])).toLowerCase();
+    let reversedInput = invertString(sanitizedInput);
+
+    return (sanitizedInput === reversedInput)
+
+    function invertString(inputString) {
+        return inputString.split('').reverse().join('')
+    }
+
+    function removeSpaces(inputString) {
+        return inputString.split(' ').join('');
+    }
+
+    function removePunctuation(inputString) {
+        var punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+        var rawLetters = inputString.split('');
+        var cleanLetters = rawLetters.filter(function(letter) {
+            return punctuation.indexOf(letter) === -1;
+        });
+        var cleanString = cleanLetters.join('');
+        return cleanString;
+    }
+
 };
 
 // Do not edit below this line
