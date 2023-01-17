26341c26b62a66b73840d5cd4a502eeefcc49d03
Andre Leitzke <355407+l33tzke@users.noreply.github.com>
2023-01-17 15:33:04 +0100

    Add solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,24 @@
 const findTheOldest = function() {
+    let people = arguments[0];
+    let oldest = people.reduce(getOldest)
+
+    return oldest; 
+
+    function getOldest(current, candidate) {
+        let currentAge = getAge(current);
+        let candidateAge = getAge(candidate);
+
+        return candidateAge > currentAge ? candidate : current;
+    }
+
+    function getAge(person) {
+        let maxLivingAge = new Date().getFullYear();
+        if (person.yearOfDeath != null) {
+            maxLivingAge = person.yearOfDeath;
+        } 
+
+        return maxLivingAge - person.yearOfBirth;
+    }
 
 };
 
